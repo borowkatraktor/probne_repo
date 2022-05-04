9cdb428d830e23c39f2a0b464606c2a36025632e
borowkatraktor <104886549+borowkatraktor@users.noreply.github.com>
2022-05-04 08:25:40 +0200

    Update pierwszy.py

--- pierwszy.py
+++ pierwszy.py
@@ -1,8 +1,6 @@
 print('hej, tu nowe repo')
 
 
-
-
 def merged(list1, list2):
     list1.extend(list2)
     for element in list1[:]:
@@ -14,3 +12,14 @@ list1 = [1, 2, 3, 4, 5, 6]
 list2 = [4, 5, 6, 7, 8, 9]
 
 print(merged(list1, list2))
+
+
+def factorial(keyin):
+    if keyin == 1 or keyin == 0:
+        return 1
+    elif keyin < 0:
+        return 'no tak to ziomek nie działa'
+    else:
+        return keyin * factorial(keyin - 1)
+
+print(factorial(int(input('jaką liczbę kurwa ten? '))))
